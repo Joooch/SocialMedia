f36bc728c83737ffc4fe45d1e93646cb05b354da
Joooch <eldar4163@gmail.com>
2022-04-25 23:19:02 +0300

    change page layout

--- client-frontend/src/pages/ProfileSetup.tsx
+++ client-frontend/src/pages/ProfileSetup.tsx
@@ -1,10 +1,13 @@
+import { Box } from '@mui/material';
 import ProfileSettings from 'features/profile-settings/ui';
 
 function ProfileSetupPage() {
     return (
-        <div className="profile-setup">
-            <h1 className='center'>Set up your profile</h1>
-            <ProfileSettings />
+        <div className='center-of-screen'>
+            <Box sx={{ maxWidth: "md" }} className="profile-setup">
+                <h1 className='center-text'>Profile Settings</h1>
+                <ProfileSettings />
+            </Box>
         </div>
     );
 }
