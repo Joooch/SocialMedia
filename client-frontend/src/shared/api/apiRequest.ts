02877f9abd29fc1e4e3a3f417c25c714066d5acc
Joooch <eldar4163@gmail.com>
2022-04-25 14:55:59 +0300

    add put request and change function order

--- client-frontend/src/shared/api/apiRequest.ts
+++ client-frontend/src/shared/api/apiRequest.ts
@@ -3,10 +3,14 @@ import axios, { AxiosRequestConfig } from "axios";
 //let backendURL = "https://localhost:7042"
 let backendURL = "" // use proxy instead
 
+export function get<T = any>(path: string, config?: AxiosRequestConfig | undefined) {
+    return axios.get<T>(backendURL + path, config)
+}
+
 export function post<T = any>(path: string, data?: any, config?: AxiosRequestConfig | undefined) {
     return axios.post<T>(backendURL + path, data, config)
 }
 
-export function get<T = any>(path: string, config?: AxiosRequestConfig | undefined) {
-    return axios.get<T>(backendURL + path, config)
+export function put<T = any>(path: string, config?: AxiosRequestConfig | undefined) {
+    return axios.put<T>(backendURL + path, config)
 }
