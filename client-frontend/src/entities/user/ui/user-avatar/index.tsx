39cbd254f7cbb06a646d4f3784941e765ec0382f
Joooch <eldar4163@gmail.com>
2022-04-25 17:54:13 +0300

    onError set default user icon

--- client-frontend/src/entities/user/ui/user-avatar/index.tsx
+++ client-frontend/src/entities/user/ui/user-avatar/index.tsx
@@ -1,8 +1,29 @@
-import Avatar from '@mui/material/Avatar';
+import './user-avatar.css';
+import { useMemo, useState } from 'react';
 import type { User } from 'shared/api/types';
 
-export const UserAvatar = (props: {user?: User, src?: string, size?: number | string}) => {
+const defaultUserImg = "/img/user.png"
+
+export const UserAvatar = (props: { user?: User, src?: string, size?: number | string }) => {
+    const [imageSrc, setImageSrc] = useState<string>(props.src ?? defaultUserImg);
+
+    useMemo(() => {
+        setImageSrc(props.src ?? defaultUserImg)
+    }, [props.src])
+
     return (
-        <Avatar className='user-avatar' alt={props.user?.firstName ?? "user-avatar"} src={props.src ?? "/img/user.png"} sx={{ width: props.size ?? 64, height: props.size ?? 64 }} />
+        <img
+            className='user-avatar'
+            alt={props.user?.firstName ?? "user-avatar"}
+            src={imageSrc}
+            width={props.size ?? 32}
+            height={props.size ?? 32}
+
+            onError={() => {
+                if (imageSrc !== defaultUserImg) {
+                    setImageSrc(defaultUserImg)
+                }
+            }}
+        />
     );
 };
