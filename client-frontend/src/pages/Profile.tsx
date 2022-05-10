67af9888f1c7692f9ce248f35a4267b4561ff0fd
Joooch <eldar4163@gmail.com>
2022-05-11 00:33:52 +0300

    init test profile page

--- client-frontend/src/pages/Profile.tsx
+++ client-frontend/src/pages/Profile.tsx
@@ -1,21 +1,44 @@
+import { Container } from "@mui/material";
 import { fetchProfile } from "entities/profile";
+import ProfileHeader from "entities/profile/ui/profile-header";
+import { useEffect, useRef, useState } from "react";
 import { useParams } from "react-router-dom";
 import { User } from "shared/api";
 
 function ProfilePage() {
-    const { id } = useParams();
+    const [user, setUser] = useState<User>();
+    const [height, setHeight] = useState<number>(64);
 
-    if(!id){
-        return <></>
+    const { id } = useParams();
+    if (!id) {
+        throw new Error("invalid params");
     }
 
+
+
     console.log("fetching")
-    fetchProfile(id)
+    useEffect(() => {
+        fetchProfile(id).then(fetchedUser => {
+            setUser(fetchedUser)
+        })
+    }, [id])
+
+    /* useEffect(() => {
+        const onScroll = e => {
+          
+        };
+        window.addEventListener("scroll", onScroll);
+    
+        return () => window.removeEventListener("scroll", onScroll);
+      }, []); */
 
     return (
-        <div className='profile-page'>
-
-        </div>
+        <Container className='profile-page' sx={{maxHeight: "20px"}}>
+            <ProfileHeader user={user} />
+            <div>
+                Content
+            </div>
+        </Container>
     );
 }
 
