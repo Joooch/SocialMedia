b732d7ea91ed0601a6c76332977589e0f44b2048
Eldar <eldar4163@gmail.com>
2023-06-12 05:07:14 +0300

    Chat messages entity

---
+++ SocialMedia.Domain/Entities/ChatMessageEntity.cs
@@ -0,0 +1,21 @@
+﻿using SocialMedia.Domain.Interfaces;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SocialMedia.Domain.Entities
+{
+    public class ChatMessageEntity : BaseEntity, ITimedEntity
+    {
+        public string Content { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+
+        [ForeignKey(nameof(Owner))]
+        public Guid OwnerId { get; set; }
+        public ProfileEntity Owner { get; set; }
+
+
+        [ForeignKey(nameof(Target))]
+        public Guid TargetId { get; set; }
+        public ProfileEntity Target { get; set; }
+    }
+}
